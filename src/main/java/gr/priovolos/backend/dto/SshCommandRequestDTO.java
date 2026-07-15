ee69c2116115a312d13a69ec40ad05f79371054c
ioannispriovolos <priovolosg@hotmail.com>
2026-07-15 17:37:07 +0300

    chore: regex for IPv4 addresses

--- src/main/java/gr/priovolos/backend/dto/SshCommandRequestDTO.java
+++ src/main/java/gr/priovolos/backend/dto/SshCommandRequestDTO.java
@@ -3,9 +3,14 @@ package gr.priovolos.backend.dto;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record SshCommandRequestDTO(
         @NotBlank(message = "Host IP address cannot be blank")
+        @Pattern(
+                regexp = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",
+                message = "Must be a valid IPv4 address"
+        )
         String host,
 
         @Min(value = 1) @Max(value = 65535)
