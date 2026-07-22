f9483a8345fb0293403064fbbb312bba62bb520f
ioannispriovolos <priovolosg@hotmail.com>
2026-07-23 01:49:39 +0300

    chore: annotations added

--- src/main/java/gr/priovolos/backend/BackendApplication.java
+++ src/main/java/gr/priovolos/backend/BackendApplication.java
@@ -2,8 +2,14 @@ package gr.priovolos.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableRetry
+@EnableAsync
 public class BackendApplication {
 
 	public static void main(String[] args) {
